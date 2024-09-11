bb33093ffd80e8c795378816b329b45a9e821dc9
ckiersten <kiersten.chou@gmail.com>
2024-09-11 13:30:30 -0700

    Add files via upload

---
+++ problem set 1B/Generators.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Generators {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        /*
+        //celsius to fahrenheit
+        System.out.println("*** Celsius --> Fahrenheit ***");
+        System.out.print("Enter a temperature in Celsius: ");
+
+        double c = input.nextDouble();
+        double f = c * (9.0/5.0) + 32;
+
+        System.out.println(c + "° Celsius = " + f + "° Fahrenheit");
+
+        //BMI calculator
+        System.out.println("*** BMI Calculator ***");
+        System.out.print("Enter a weight in kg: ");
+
+        double weight = input.nextDouble();
+
+        System.out.print("Enter a height in m: ");
+
+        double height = input.nextDouble();
+
+        double BMI = weight/(height*height);
+
+        System.out.print("A " + height + "m tall adult who weighs " 
+        + weight + "kg has a BMI of " + BMI);
+
+        //days to weeks and days
+        System.out.println("*** Days --> Weeks and Days ***");
+        System.out.print("Enter a number of days: ");
+
+        int totalDays = input.nextInt();
+        int weeks = totalDays/7;
+        int remainDays = totalDays%7;
+
+        System.out.print(totalDays + " days is equal to " + weeks + 
+        " weeks, " + remainDays + " days.");
+
+        //receipt generator
+        System.out.println("*** Receipt Generator ***");
+        System.out.print("Number of units you're buying: ");
+
+        int units = input.nextInt();
+
+        System.out.print("Price per unit: ");
+
+        double price = input.nextDouble();
+
+        System.out.print("Tax rate: ");
+
+        double tax = input.nextDouble();
+
+        double total = (units*price)*((tax/100)+1);
+
+        System.out.print("Purchasing " + units + " units at $" + price + " with " + tax + "% tax will cost $" + total);
+         */
+        //sum of digits
+        System.out.println("*** Sum of digits ***");
+        System.out.print("Enter a positive, three-digit number: ");
+
+        int number = input.nextInt();
+
+        int quotient = number/10;
+        int remainder = number%10;
+        quotient = quotient/10;
+        remainder = remainder + (quotient%10);
+        quotient = quotient/10;
+        remainder = remainder + (quotient%10);
+        
+        int sum = quotient + remainder;
+
+        System.out.print("The sum of the digits of " + number + " is " + sum);
+    }
+}
