bb0d6dcbfd65464f7fb6b6de56bc5901209e0d0f
ckiersten <kiersten.chou@gmail.com>
2024-09-11 17:48:20 -0700

    finished sum of digits

--- problem set 1B/Generators.java
+++ problem set 1B/Generators.java
@@ -4,75 +4,87 @@ public class Generators {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        /*
-        //celsius to fahrenheit
-        System.out.println("*** Celsius --> Fahrenheit ***");
-        System.out.print("Enter a temperature in Celsius: ");
+        
+        // //celsius to fahrenheit
+        // System.out.println("*** Celsius --> Fahrenheit ***");
+        // System.out.print("Enter a temperature in Celsius: ");
 
-        double c = input.nextDouble();
-        double f = c * (9.0/5.0) + 32;
+        // double c = input.nextDouble();
+        // double f = c * (9.0/5.0) + 32;
 
-        System.out.println(c + "° Celsius = " + f + "° Fahrenheit");
+        // System.out.println(c + "° Celsius = " + f + "° Fahrenheit");
 
-        //BMI calculator
-        System.out.println("*** BMI Calculator ***");
-        System.out.print("Enter a weight in kg: ");
+        // //BMI calculator
+        // System.out.println("*** BMI Calculator ***");
+        // System.out.print("Enter a weight in kg: ");
 
-        double weight = input.nextDouble();
+        // double weight = input.nextDouble();
 
-        System.out.print("Enter a height in m: ");
+        // System.out.print("Enter a height in m: ");
 
-        double height = input.nextDouble();
+        // double height = input.nextDouble();
 
-        double BMI = weight/(height*height);
+        // double BMI = weight/(height*height);
 
-        System.out.print("A " + height + "m tall adult who weighs " 
-        + weight + "kg has a BMI of " + BMI);
+        // System.out.print("A " + height + "m tall adult who weighs " 
+        // + weight + "kg has a BMI of " + BMI);
 
-        //days to weeks and days
-        System.out.println("*** Days --> Weeks and Days ***");
-        System.out.print("Enter a number of days: ");
+        // //days to weeks and days
+        // System.out.println("*** Days --> Weeks and Days ***");
+        // System.out.print("Enter a number of days: ");
 
-        int totalDays = input.nextInt();
-        int weeks = totalDays/7;
-        int remainDays = totalDays%7;
+        // int totalDays = input.nextInt();
+        // int weeks = totalDays/7;
+        // int remainDays = totalDays%7;
 
-        System.out.print(totalDays + " days is equal to " + weeks + 
-        " weeks, " + remainDays + " days.");
+        // System.out.print(totalDays + " days is equal to " + weeks + 
+        // " weeks, " + remainDays + " days.");
 
-        //receipt generator
-        System.out.println("*** Receipt Generator ***");
-        System.out.print("Number of units you're buying: ");
+        // //receipt generator
+        // System.out.println("*** Receipt Generator ***");
+        // System.out.print("Number of units you're buying: ");
 
-        int units = input.nextInt();
+        // int units = input.nextInt();
 
-        System.out.print("Price per unit: ");
+        // System.out.print("Price per unit: ");
 
-        double price = input.nextDouble();
+        // double price = input.nextDouble();
 
-        System.out.print("Tax rate: ");
+        // System.out.print("Tax rate: ");
 
-        double tax = input.nextDouble();
+        // double tax = input.nextDouble();
 
-        double total = (units*price)*((tax/100)+1);
+        // double total = (units*price)*((tax/100)+1);
 
-        System.out.print("Purchasing " + units + " units at $" + price + " with " + tax + "% tax will cost $" + total);
-         */
+        // System.out.print("Purchasing " + units + " units at $" + price + " with " + tax + "% tax will cost $" + total);
+         
         //sum of digits
         System.out.println("*** Sum of digits ***");
         System.out.print("Enter a positive, three-digit number: ");
 
         int number = input.nextInt();
-
-        int quotient = number/10;
-        int remainder = number%10;
-        quotient = quotient/10;
-        remainder = remainder + (quotient%10);
-        quotient = quotient/10;
-        remainder = remainder + (quotient%10);
         
-        int sum = quotient + remainder;
+        // int r = number%10;
+        // int q = number/10;
+        // System.out.println(r);
+        // System.out.println(q);
+        // r += (q%10);
+        // q = q/10;
+        // System.out.println(r);
+        // System.out.println(q);
+        // r += (q%10);
+        // q = q/10;
+        // System.out.println(r);
+        // System.out.println(q);
 
-        System.out.print("The sum of the digits of " + number + " is " + sum);
+        int r = 0;
+        int q = number;
+        while (q > 0) 
+        {
+            r += q%10;
+            q = q/10;
+        }
+       
+        System.out.print("The sum of the digits of " + number + " is " + r);
     }
 }
