a20ceb0cd305744ffa9fc980b68db42e876bdf86
ckiersten <kiersten.chou@gmail.com>
2025-01-08 15:01:23 -0800

    Add files via upload

---
+++ problem set 5A/Driver.java
@@ -0,0 +1,18 @@
+//Kiersten Chou, 1/8/25
+
+class Driver {
+    
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(2,4);
+        /*System.out.println(f1.getNum());
+        System.out.println(f1.getDenom());
+        System.out.println(f1.toString());
+        System.out.println(f1.toDouble());
+        f1.reduce();
+        System.out.println(f1.toString());
+        */
+       //f1.multiply(f1, new Fraction (3, 4));
+       System.out.println(f1.multiply(f1, new Fraction (3, 4)));
+       
+    }
+}
--- problem set 5A/Fraction.java
+++ problem set 5A/Fraction.java
@@ -1,14 +1,68 @@
+//Kiersten Chou, 1/8/25
+
 public class Fraction {
 
     private int numerator, denominator;
 
-    public Fraction () {
+    public Fraction() {
         numerator = 1;
         denominator = 1;
     }
 
-    public Fraction (int n, int d) {
+    public Fraction(int n, int d) {
         numerator = n;
+        setDenom(d);
+    }
+
+    public Fraction(String str) {
+        numerator = Integer.parseInt(str.substring(0, str.indexOf("/")));
+        if (Integer.parseInt(str.substring(str.indexOf("/")+1)) == 0) {
+            System.out.println("Error: denominator cannot be zero.");
+            denominator = 1;
+        } else {
+            denominator = Integer.parseInt(str.substring(str.indexOf("/")+1));
+        }
+    }
+
+    public Fraction(Fraction f) {
+        numerator = f.numerator;
+        denominator = f.denominator;
+    }
+
+    public int getNum() {
+        return numerator;
+    }
+
+    public int getDenom() {
+        return denominator;
+    }
+
+    public String toString() {
+        return numerator +"/"+ denominator;
+    }
+
+    public double toDouble() {
+        return (double) numerator / denominator;
+    }
+
+    private int greatestCommonFactor(int n1, int n2) {
+        if (n1 == n2) return n1;
+        int larger = Math.max(n1, n2);
+        int smaller = Math.min(n1, n2);
+        return greatestCommonFactor((larger - smaller), smaller);
+    }
+
+    public void reduce() {
+        int gcf = greatestCommonFactor(numerator, denominator);
+        numerator = numerator / gcf;
+        denominator = denominator / gcf;
+    }
+    
+    public void setNum(int n) {
+        numerator = n;
+    }
+    
+    public void setDenom(int d) {
         if (d == 0) {
             System.out.println("Error: denominator cannot be zero.");
             d = 1;
@@ -16,14 +70,16 @@ public class Fraction {
             denominator = d;
         }
     }
-
-    public Fraction (String str) {
-        numerator = Integer.parseInt(str.substring(0, str.indexOf("/")));
-        denominator = Integer.parseInt(str.substring(str.indexOf("/")));
+    
+    public static Fraction multiply(Fraction f1, Fraction f2) {
+        int n = f1.numerator * f2.numerator;
+        int d = f1.denominator * f2.denominator;
+        return new Fraction(n, d);
     }
-
-    public Fraction (Fraction f) {
-        numerator = f.numerator;
-        denominator = f.denominator;
+    
+    public static Fraction divide(Fraction f1, Fraction f2) {
+        int n = f1.numerator * f2.denominator;
+        int d =  f1.denominator * f2.numerator;
+        return new Fraction(n, d);
     }
 }
